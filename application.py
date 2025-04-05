faffd4b84b552cb7b89671f64b2610c24d3a4af3
VAIBHAV GARG <vaibhgarg5@gmail.com>
2025-04-05 13:39:29 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonify,render_template,request
+from flask import Flask,render_template,request
 import pickle
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
